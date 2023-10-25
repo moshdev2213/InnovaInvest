f3a4e1de1aa6316c544103584ea65382fc5f865b
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-10-26 00:56:53 +0530

    almostInInvestor

---
+++ frontend/Invest/app/src/main/java/com/example/invest/Activity/MyProporsals.kt
@@ -0,0 +1,12 @@
+package com.example.invest.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.invest.R
+
+class MyProporsals : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_proporsals)
+    }
+}
