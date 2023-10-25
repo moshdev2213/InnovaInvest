f86bf39acb917112b584905b03d2cf7bb846263f
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-10-25 22:00:35 +0530

    doneEditproject

---
+++ frontend/Innova/app/src/main/java/com/example/innova/Activity/EditProject.kt
@@ -0,0 +1,12 @@
+package com.example.innova.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.innova.R
+
+class EditProject : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_project)
+    }
+}
