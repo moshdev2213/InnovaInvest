2e170d550044913b70cb99a4f7a8d1056615bf72
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-10-26 15:12:38 +0530

    myProjDetailDone

---
+++ frontend/Invest/app/src/main/java/com/example/invest/Activity/SendProporsal.kt
@@ -0,0 +1,12 @@
+package com.example.invest.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.invest.R
+
+class SendProporsal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_proposal)
+    }
+}
