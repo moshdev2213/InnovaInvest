071574fc2613bdb497e5af04236b412587ab1de0
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-10-26 21:26:51 +0530

    navigationInHomeDne

--- frontend/Innova/app/src/main/java/com/example/innova/Fragment/IndexFragment.kt
+++ frontend/Innova/app/src/main/java/com/example/innova/Fragment/IndexFragment.kt
@@ -1,60 +1,85 @@
 package com.example.innova.Fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.example.innova.EntityRes.UserRecord
 import com.example.innova.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [IndexFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class IndexFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private var out: UserRecord? = null
+    private lateinit  var btnSeeWrk:Button
+    private lateinit  var btnBrowseReserves:Button
+    private lateinit  var btnBrowseProjects02:Button
+    private lateinit  var btnShowProjects:Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_index, container, false)
+        val view = inflater.inflate(R.layout.fragment_index, container, false)
+
+        bottomNavigationView =  requireActivity().findViewById(R.id.bottomNavigationView)
+        btnSeeWrk =  view.findViewById(R.id.btnSeeWrk)
+        btnBrowseReserves =  view.findViewById(R.id.btnBrowseReserves)
+        btnBrowseProjects02 = view.findViewById(R.id.btnBrowseProjects02)
+        btnShowProjects =  view.findViewById(R.id.btnShowProjects)
+
+        out = requireArguments().getSerializable("user") as UserRecord?
+
+        btnShowProjects.setOnClickListener(View.OnClickListener {
+            reDirectAccountPage()
+            bottomNavigationView.selectedItemId = R.id.profile
+        })
+
+        btnBrowseReserves.setOnClickListener(View.OnClickListener {
+            reDirectAccountPage()
+            bottomNavigationView.selectedItemId = R.id.project
+        })
+
+        btnSeeWrk.setOnClickListener(View.OnClickListener {
+            reDirectAccountPage()
+            bottomNavigationView.selectedItemId = R.id.work
+        })
+
+        btnBrowseProjects02.setOnClickListener(View.OnClickListener {
+            reDirectAccountPage()
+            bottomNavigationView.selectedItemId = R.id.project
+        })
+
+        return view
+    }
+    private fun reDirectAccountPage() {
+        replaceFrag(ProfileFragment(), out)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment IndexFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            IndexFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun reDirectProjectsPage() {
+        replaceFrag(ProjectFragment(), out)
     }
+
+    private fun reWorkPage() {
+        replaceFrag(WorkFragment(), out)
+    }
+
+    private fun replaceFrag(fragment: Fragment, out: UserRecord?) {
+        val transaction: FragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
+        if (out != null) {
+            val bundle = Bundle()
+            bundle.putSerializable("user", out)
+            fragment.arguments = bundle
+        }
+        transaction.replace(
+            R.id.frame_layout,
+            fragment
+        ) // Replace the current Fragment with FragmentB
+        transaction.commit()
+    }
+
 }
