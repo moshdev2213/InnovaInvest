cea08b6c6762362039058979e49dffe9ee96ba49
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-10-27 02:44:25 +0530

    CrudForProjectPopo2go

--- frontend/Innova/app/src/main/java/com/example/innova/Activity/MyProporsals.kt
+++ frontend/Innova/app/src/main/java/com/example/innova/Activity/MyProporsals.kt
@@ -1,12 +1,165 @@
 package com.example.innova.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.charmrides.RetrofitService.RetrofitService
+import com.example.innova.Adapter.ProjectsAdapter
+import com.example.innova.ApiService.ProjectService
+import com.example.innova.DialogAlerts.ApiProgress
+import com.example.innova.EntityRes.ProjTotalBudget
+import com.example.innova.EntityRes.ProjectItem
+import com.example.innova.EntityRes.ProjectsRes
+import com.example.innova.EntityRes.UserRecord
 import com.example.innova.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyProporsals : AppCompatActivity() {
+    private var out: UserRecord? = null
+    private lateinit var rvMyProProporsals:RecyclerView
+    private lateinit var projectsAdapter:ProjectsAdapter
+    private lateinit var apiProgress: ApiProgress
+    private lateinit var fabAddMyProj: FloatingActionButton
+    private lateinit var imgBackToProfileFromMy: ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_proporsals)
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            // Retrieve the user and bus items
+            out = bundle.getSerializable("user") as UserRecord
+        }
+
+        rvMyProProporsals = findViewById(R.id.rvMyProProporsals)
+        fabAddMyProj = findViewById(R.id.fabAddMyProj)
+        imgBackToProfileFromMy = findViewById(R.id.imgBackToProfileFromMy)
+
+        fabAddMyProj.setOnClickListener {
+            val bundle = Bundle()
+            bundle.putSerializable("user", out)
+
+            val intent = Intent(this@MyProporsals, AddProject::class.java)
+            intent.putExtras(bundle)
+            startActivity(intent)
+        }
+        imgBackToProfileFromMy.setOnClickListener {
+            finish()
+        }
+
+        initRecycler()
+    }
+    private fun initRecycler(){
+
+        rvMyProProporsals.layoutManager= LinearLayoutManager(this@MyProporsals)
+        projectsAdapter = ProjectsAdapter ({
+                projectItem: ProjectItem -> projCardClicked(projectItem)
+        },this@MyProporsals )
+        rvMyProProporsals.adapter = projectsAdapter
+        fetchDetails()
+
+        rvMyProProporsals.visibility = View.GONE
+    }
+
+    private fun projCardClicked(projectItem: ProjectItem) {
+        val bundle = Bundle()
+        bundle.putSerializable("user", out)
+        bundle.putSerializable("project", projectItem)
+
+        val intent = Intent(this@MyProporsals, ProjectDetail::class.java)
+        intent.putExtras(bundle)
+        startActivity(intent)
+    }
+    private fun fetchDetails() {
+        apiProgress = ApiProgress(this@MyProporsals)
+        apiProgress.startProgressLoader()
+
+        val emailToFilter: String = out?.record?.email ?: ""
+        val filterValue = "(email=\"$emailToFilter\")"
+
+        val retrofitService= RetrofitService()
+        val getList =retrofitService.getRetrofit().create(ProjectService::class.java)
+        val call: Call<ProjectsRes> = getList.getProjNotUser(filterValue)
+
+        call.enqueue(object : Callback<ProjectsRes> {
+            override fun onResponse(call: Call<ProjectsRes>, response: Response<ProjectsRes>) {
+                if(response.isSuccessful){
+                    if (response.body()!=null){
+                        println(response.body())
+                        val mealRes = response.body()
+                        val mealItem = mealRes?.items
+
+                        getProjTotBudById()
+                        projectsAdapter.setList(mealItem!!)
+                        rvMyProProporsals.visibility= View.VISIBLE
+                        apiProgress.dismissProgressLoader()
+                    }else{
+                        Toast.makeText(this@MyProporsals,"Empty Data", Toast.LENGTH_SHORT).show()
+                        apiProgress.dismissProgressLoader()
+                    }
+
+                }else{
+                    Toast.makeText(this@MyProporsals,"Invalid response", Toast.LENGTH_SHORT).show()
+                    apiProgress.dismissProgressLoader()
+                }
+            }
+            override fun onFailure(call: Call<ProjectsRes>, t: Throwable) {
+                Toast.makeText(this@MyProporsals,"Server Error", Toast.LENGTH_SHORT).show()
+                apiProgress.dismissProgressLoader()
+            }
+        })
+    }
+    private fun getProjTotBudById() {
+//        val filterValue = "(forProject.id=\"$id\")"
+        val expand = "forProject.budget,forProject.id"
+        val fields = "budget,forProject"
+
+        val retrofitService= RetrofitService()
+        val getList =retrofitService.getRetrofit().create(ProjectService::class.java)
+        val call: Call<ProjTotalBudget> = getList.getProjTotBudById(expand,fields)
+
+        call.enqueue(object : Callback<ProjTotalBudget> {
+            override fun onResponse(call: Call<ProjTotalBudget>, response: Response<ProjTotalBudget>) {
+                if(response.isSuccessful){
+                    if (response.body()!=null){
+                        println(response.body())
+                        val mealRes = response.body()
+                        val mealItem = mealRes?.items
+
+//                        val keyValuePairs = mealItem?.map { item ->
+//                            Pair(item.forProject,item.budget)
+//                        }
+                        // Assuming mealItem is a list of your data items
+                        val groupedItems = mealItem?.groupBy { it.forProject }
+
+                        val keyValuePairs = groupedItems?.map { (project, items) ->
+                            val totalBudget = items.sumOf { it.budget }
+                            Pair(project, totalBudget)
+                        }
+
+                        if (keyValuePairs != null) {
+                            projectsAdapter.setKeyValuePairs(keyValuePairs)
+                        }
+                    }else{
+                        Toast.makeText(this@MyProporsals,"Empty Data", Toast.LENGTH_SHORT).show()
+                    }
+                }else{
+                    Toast.makeText(this@MyProporsals,"Invalid response", Toast.LENGTH_SHORT).show()
+                }
+            }
+            override fun onFailure(call: Call<ProjTotalBudget>, t: Throwable) {
+                Toast.makeText(this@MyProporsals,"Server Error", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
--- frontend/Innova/app/src/main/java/com/example/innova/Fragment/ProfileFragment.kt
+++ frontend/Innova/app/src/main/java/com/example/innova/Fragment/ProfileFragment.kt
@@ -1,60 +1,44 @@
 package com.example.innova.Fragment
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.cardview.widget.CardView
+import com.example.innova.Activity.MyProporsals
+import com.example.innova.Activity.ProjectDetail
+import com.example.innova.EntityRes.UserRecord
 import com.example.innova.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var out: UserRecord? = null
+    private lateinit var cvProceedToMyProj:CardView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
+        var view =  inflater.inflate(R.layout.fragment_profile, container, false)
+        out = arguments?.getSerializable("user", UserRecord::class.java)!!
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        cvProceedToMyProj = view.findViewById(R.id.cvProceedToMyProj)
+
+        cvProceedToMyProj.setOnClickListener {
+            val bundle = Bundle()
+            bundle.putSerializable("user", out)
+
+            val intent = Intent(requireActivity(), MyProporsals::class.java)
+            intent.putExtras(bundle)
+            startActivity(intent)
+        }
+
+        return view
     }
 }
